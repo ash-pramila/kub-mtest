c4d5b7d0219351aa6fc79ed7621938dbed49ef58
pramila-git <36467622+pramila-git@users.noreply.github.com>
2018-08-14 09:00:14 +0530

    Update SeleniumMain

--- sele-kub/src/main/java/demojar/SeleniumMain.java
+++ sele-kub/src/main/java/demojar/SeleniumMain.java
@@ -9,7 +9,7 @@ public class SeleniumMain {
 	public static void main(String[] args) {
 		JUnitCore junit = new JUnitCore();
 		  junit.addListener(new TextListener(System.out));
-		  Result result = junit.run(SimpleKubSeleniumGridTest.class); // Replace "Docker_p" with the name of your class
+		  Result result = junit.run(MultipleBrowser.class); // Replace "Docker_p" with the name of your class
 		  if (result.getFailureCount() > 0) {
 		    System.out.println("Test failed.");
 		    System.exit(1);
